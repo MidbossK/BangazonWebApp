0fb1d8bd0780044351bc53aa042f088fcd6f94d6
Keith <MidbossKeith@gmail.com>
2018-02-16 14:19:51 -0600

    startup

---
+++ BangazonWebApp/Models/ApplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonWebApp.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        public string Zip { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ BangazonWebApp/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWebApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Description;
+
+        [Required]
+        [Display(Name = "Available")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Display(Name = "Created on")]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [Display(Name = "Product Category")]
+        public int ProductTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+    }
+}
