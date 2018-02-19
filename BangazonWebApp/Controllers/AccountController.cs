21d5b9b6a90c956b9617fb8c101e24a14bfabcc1
Keith <midbosskeith@gmail.com>
2018-02-19 10:21:20 -0600

    New user creation is so gucci right nao.

--- BangazonWebApp/Controllers/AccountController.cs
+++ BangazonWebApp/Controllers/AccountController.cs
@@ -220,7 +220,16 @@ namespace BangazonWebApp.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Address = model.Address,
+                    City = model.City,
+                    State = model.State,
+                    Zip = model.Zip,
+                };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
